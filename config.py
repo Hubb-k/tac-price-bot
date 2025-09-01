ef50b9dba5cc4f22faa239b9905b3892ac78d874
Hubb-k <kerim.mquliyev@gmail.com>
2025-09-01 17:52:53 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,17 @@
+# Конфигурация чатов
+CHAT_ID = "-1002954606074"
+ADMIN_CHAT_ID = "224780379"
+
+# Конфигурация токена
+JETTON_ADDRESS = "EQBE_gBrU3mPI9hHjlJoR_kYyrhQgyCFD6EUWfa42W8T7EBP"
+
+# GIF-анимации
+GIF_URLS = [
+    "https://media.giphy.com/media/83JLhFcYedwQBR0oW5/giphy.mp4",
+    "https://media.giphy.com/media/nZW5fIzaIJVXddVla7/giphy.mp4",
+    "https://media.giphy.com/media/KNeeNrAQQqt0v8jvf9/giphy.mp4"
+]
+
+# Общие переменные
+message_counters = {}
+MAX_HISTORY_SIZE = 16
