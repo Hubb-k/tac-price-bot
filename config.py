1fee152c87432d6da25d7373c2889c82ad542810
Hubb-k <kerim.mquliyev@gmail.com>
2025-09-02 08:29:12 +0300

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ JETTON_ADDRESS = "EQBE_gBrU3mPI9hHjlJoR_kYyrhQgyCFD6EUWfa42W8T7EBP"
 GIF_URLS = [
     "https://media.giphy.com/media/83JLhFcYedwQBR0oW5/giphy.mp4",
     "https://media.giphy.com/media/nZW5fIzaIJVXddVla7/giphy.mp4",
-"https://media.giphy.com/media/POHHHBbtATeVA4qMY6.giphy.mp4",
+    "https://media.giphy.com/media/POHHHBbtATeVA4qMY6/giphy.mp4",
     "https://media.giphy.com/media/KNeeNrAQQqt0v8jvf9/giphy.mp4"
 ]
 
